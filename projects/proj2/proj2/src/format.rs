4096d607f95badfdda49267420fb313eb098c2b3
Franciszek Stachura <fbstachura@gmail.com>
2025-11-10 17:05:40 +0100

    refactor, separate i/o from parsing

---
+++ projects/proj2/proj2/src/format.rs
@@ -0,0 +1,244 @@
+use std::string::FromUtf8Error;
+use std::io::{BufRead, BufReader, Read, Write};
+
+use zstd::stream::{Decoder, Encoder};
+
+pub use crate::utils::{decode_vle, decode_vle_vec, encode_vle, encode_i64_vle, decode_i64_vle, read_strings, read_numbers};
+
+const ZSTD_COMPRESSION_LEVEL: i32 = 3;
+pub const MAGIC: [u8; 4] = ['I' as u8, 'S' as u8, 'B' as u8, 'D' as u8];
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ColumnType {
+    Int64 = 0,
+    Str = 1,
+}
+
+impl TryFrom<u32> for ColumnType {
+    type Error = ();
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ColumnType::Int64),
+            1 => Ok(ColumnType::Str),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Into<u32> for ColumnType {
+    fn into(self) -> u32 {
+        match self {
+            ColumnType::Int64 => 0,
+            ColumnType::Str => 1,
+        }
+    }
+}
+
+// file format:
+// magic: u32 (ISBD)
+// column type: u32
+// number of chunks: u64
+// (chunks start)
+//
+// for int64:
+// chunk len in bytes: u64
+// chunk len in numbers: u64
+// first num: i64
+// (data)
+//
+// for string:
+// chunk len in bytes: u64
+// chunk len in rows: u64
+// zstd compressed data:
+// string len: u32
+// string: [u8]
+
+#[derive(Debug)]
+pub enum DeserializerError {
+    InvalidMagic,
+    InvalidColumnType,
+    InvalidInt64,
+    TooShort,
+    InvalidString(FromUtf8Error),
+    IOError(std::io::Error),
+}
+
+pub const HEADER_SIZE: usize = 2 * size_of::<u32>() + size_of::<u64>();
+
+pub fn create_header(writer: &mut impl Write, datatype: ColumnType, number_of_chunks: u64) -> std::io::Result<()> {
+    writer.write(&MAGIC)?;
+    writer.write(&(Into::<u32>::into(datatype)).to_be_bytes())?;
+    writer.write(&number_of_chunks.to_be_bytes())?;
+    Ok(())
+}
+
+pub fn parse_header(buf: &[u8; HEADER_SIZE]) -> Result<(ColumnType, u64), DeserializerError> {
+    if buf[0..4] != MAGIC {
+        return Err(DeserializerError::InvalidMagic);
+    }
+
+    // check if type matches
+    let ctype: ColumnType = u32::from_be_bytes(
+            buf[4..8].try_into().or(Err(DeserializerError::TooShort))?
+        )
+        .try_into().or(Err(DeserializerError::InvalidColumnType))?;
+
+    // read number of chunks
+    let num_of_chunks = u64::from_be_bytes(buf[8..16].try_into().unwrap());
+
+    Ok((ctype, num_of_chunks))
+}
+
+pub const CHUNK_HEADER_SIZE: usize = 2 * size_of::<u64>();
+
+pub fn parse_chunk_header(buf: &[u8; CHUNK_HEADER_SIZE]) -> Result<(u64, u64), DeserializerError> {
+    let bytes = u64::from_be_bytes(buf[0..8].try_into().or(Err(DeserializerError::TooShort))?);
+    let rows = u64::from_be_bytes(buf[8..16].try_into().or(Err(DeserializerError::TooShort))?);
+
+    Ok(( bytes, rows ))
+}
+
+
+// does not take a write because it needs to calculate chunk size to write the whole chunk.
+pub fn create_int64_chunk(nums: &[i64]) -> Vec<u8> {
+    let mut result = Vec::with_capacity(CHUNK_HEADER_SIZE);
+
+    // write chunk len
+    result.extend_from_slice(&(0 as u64).to_be_bytes());
+
+    // write chunk len in numbers
+    result.extend_from_slice(&(nums.len() as u64).to_be_bytes());
+
+    let min_num = nums.iter().min().map(|v| *v).or(Some(0)).unwrap();
+    result.extend_from_slice(&encode_i64_vle(min_num));
+    for num in nums {
+        // should not occur as all numbers are larger than min_num
+        let num = num.checked_sub(min_num).unwrap();
+        result.extend_from_slice(&encode_i64_vle(num));
+    }
+
+    let chunk_len = result.len()-CHUNK_HEADER_SIZE;
+    result[0..8].copy_from_slice(&(chunk_len as u64).to_be_bytes());
+
+    result
+}
+
+// only sync read - regardless of async you are meant to read the whole chunk into memory
+pub struct Int64ColumnChunkIterator<'a, T: Read> {
+    pub data: &'a mut T,
+    pub remaining_items: u64,
+    pub leader: i64,
+    pub error: Option<DeserializerError>
+}
+
+impl<'a, T: Read> Iterator for Int64ColumnChunkIterator<'a, T> {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining_items != 0 && self.error.is_none() {
+            self.remaining_items -= 1;
+            match decode_i64_vle(&mut self.data).map(|v| v.map(|v| v + self.leader)) {
+                Ok(Some(n)) => Some(n),
+                Ok(None) => {
+                    self.error = Some(DeserializerError::InvalidInt64);
+                    None
+                }
+                Err(err) => {
+                    self.error = Some(DeserializerError::IOError(err));
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+pub fn parse_int64_chunk<'a, T: Read>(rows: u64, data: &'a mut T) -> Result<Int64ColumnChunkIterator<'a, T>, DeserializerError> {
+    let leader = decode_i64_vle(data)
+        .or_else(|e| Err(DeserializerError::IOError(e)))?
+        .ok_or(DeserializerError::InvalidInt64)?;
+
+    Ok(Int64ColumnChunkIterator {
+        data: data,
+        remaining_items: rows,
+        leader: leader,
+        error: None,
+    })
+}
+
+
+pub fn create_str_chunk(strs: &[String]) -> std::io::Result<Vec<u8>> {
+    let mut result = Vec::with_capacity(CHUNK_HEADER_SIZE);
+
+    // write chunk len
+    result.extend_from_slice(&(0 as u64).to_be_bytes());
+
+    // write chunk len in rows
+    result.extend_from_slice(&(strs.len() as u64).to_be_bytes());
+
+    let mut encoder = Encoder::new(result, ZSTD_COMPRESSION_LEVEL)?;
+    // TODO: multithreaded compression? only makes sense for really large chunks
+
+    for s in strs {
+        encoder.write(&(s.len() as u32).to_be_bytes())?;
+        encoder.write(&s.as_bytes())?;
+    }
+
+    let mut result = encoder.finish()?;
+    let chunk_len = result.len() - CHUNK_HEADER_SIZE;
+
+    result[0..8].copy_from_slice(&(chunk_len as u64).to_be_bytes());
+
+    Ok(result)
+}
+
+pub struct StringColumnChunkIterator<'a, T> {
+    pub decoder: Decoder<'a, T>,
+    pub remaining_items: u64,
+    pub error: Option<DeserializerError>
+}
+
+impl<'a, T: BufRead> Iterator for StringColumnChunkIterator<'a, T> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining_items != 0 && self.error.is_none() {
+            let mut len_buf: [u8; 4] = [0; 4];
+            if let Err(err) = self.decoder.read(&mut len_buf) {
+                self.error = Some(DeserializerError::IOError(err));
+                return None
+            }
+
+            let len = u32::from_be_bytes(len_buf);
+
+            let mut buf: Vec<u8> = Vec::new();
+            buf.resize(len as usize, 0);
+            if let Err(err) = self.decoder.read(&mut buf) {
+                self.error = Some(DeserializerError::IOError(err));
+                return None
+            }
+
+            self.remaining_items -= 1;
+            match String::from_utf8(buf) {
+                Ok(s) => Some(s),
+                Err(err) => {
+                    self.error = Some(DeserializerError::InvalidString(err));
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+pub fn parse_str_chunk(rows: u64, data: &mut impl Read) -> Result<StringColumnChunkIterator<'_, BufReader<&mut impl Read>>, DeserializerError> {
+    Ok(StringColumnChunkIterator {
+        decoder: Decoder::new(data).unwrap(),
+        remaining_items: rows,
+        error: None,
+    })
+}
+
